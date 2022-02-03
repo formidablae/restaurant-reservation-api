1b166fa087c95d533c00db2a69ecae0e5f9753b9
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-04 00:17:05 +0100

    Creating User entity

---
+++ src/app/modules/entities/User.ts
@@ -0,0 +1,45 @@
+import * as bcrypt from 'bcryptjs';
+import { IsNotEmpty, Length } from 'class-validator';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Unique,
+    UpdateDateColumn
+} from 'typeorm';
+
+@Entity()
+@Unique(['email'])
+export class User {
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column()
+    @IsNotEmpty()
+    public name!: string;
+
+    @Column()
+    @Length(4, 100)
+    public email!: string;
+
+    @Column()
+    @Length(4, 100)
+    public password!: string;
+
+    @Column()
+    @CreateDateColumn()
+    public createdAt!: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    public updatedAt!: Date;
+
+    public hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8);
+    }
+
+    public checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+        return bcrypt.compareSync(unencryptedPassword, this.password);
+    }
+}
