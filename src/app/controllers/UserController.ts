07a00982c33344920624f02058721d8935b2e733
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-05 19:22:40 +0100

    Created new API route for user creation. Updated interface, added the new needed methods in UserController and UserService

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -13,6 +13,16 @@ class UserController {
             next(new Error('Error occurred')); // TODO: handle error
         });
     }
+
+    public static addNew = (req: Request, res: Response, next: NextFunction) => {
+        userService.add(req.body).then((data: any) => {
+            if (data) {
+                res.json('User saved succesfully');
+            }
+        }).catch((err: Error) => {
+            next(new Error('Error occurred')); // TODO: handle error
+        })
+    }
 }
 
 export default UserController;
--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { User } from "../models/entities/User";
+import { IUser } from '../models/User';
 import { IUserRepository } from "./UserRepository";
 
 export class UserService implements IUserRepository {
@@ -13,6 +14,47 @@ export class UserService implements IUserRepository {
         }
         catch (error) {
             return null  // TODO: handle errors
+        }
+    }
+
+    async getById(id: number): Promise<User | null> {
+
+        const userRepository = getRepository(User);
+        try {
+            const user = await userRepository.findOneOrFail(id);
+            return user;
+        } catch (error) {
+            return null;  // TODO: handle errors
+        }
+    }
+
+    async add(model: IUser): Promise<User | null> {
+        const { name, email, password } = model;
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = password;
+        const userRepository = getRepository(User);
+        try {
+            const savedUser = await userRepository.save(user);
+            return savedUser;
+        } catch (e) {
+            console.log(e);
+            return Promise.reject(new Error('User with email: ' + email + ' already exists'));  // TODO: handle errors
+        }
+    }
+
+    async delete(id: number): Promise<User | null> {
+        const userRepository = getRepository(User);
+        let user: User;
+        try {
+            user = await userRepository.findOneOrFail(id);
+            if (user) {
+                userRepository.delete(id);
+            }
+            return null;
+        } catch (error) {
+            return null;  // TODO: handle errors
         }
     }
 }
