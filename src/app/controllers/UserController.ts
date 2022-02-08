c0c7a0a7b7fa5a8db399cb7c7dfeb1e30b49b006
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-08 02:38:03 +0100

    Fixed UserController to return a message when there are no users found

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import { UserService } from '../services/UserService';
+import { UsersService } from '../services/users.service';
 
-const userService = new UserService();
+const usersService = new UsersService();
 
 class UserController {
     public static listAll(req: Request, res: Response, next: NextFunction) {
-        userService.get().then((data: any) => {
+        usersService.get().then((data: any) => {
             if (data && data.length > 0) {
                 res.status(200).json(data);
+            }
+            else {
+                res.status(404).json({ message: 'No users found' });
             }
         }).catch((err: Error) => {
             next(new Error('Error occurred')); // TODO: handle error
@@ -15,7 +18,7 @@ class UserController {
     }
 
     public static addNew = (req: Request, res: Response, next: NextFunction) => {
-        userService.add(req.body).then((data: any) => {
+        usersService.add(req.body).then((data: any) => {
             if (data) {
                 res.json('User saved succesfully');
             }
