1642b7cd20380860483b7da03baa048ce7eda191
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-08 22:08:25 +0100

    Implemented functionality to get restaurants filtering by fully booked, not fully booked or all, from and to datetime, with pagination and sorting. Get and delete restaurant by id

--- src/app/controllers/RestaurantController.ts
+++ src/app/controllers/RestaurantController.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import { Restaurant } from '../models/entities/Restaurant';
 import { RestaurantsService } from '../services/restaurants.service';
 
 const restaurantsService = new RestaurantsService();
 
 class RestaurantController {
-    public static listAll(req: Request, res: Response, next: NextFunction) {
-        restaurantsService.get().then((data: any) => {
+
+    public static getFiltered = (req: Request, res: Response, next: NextFunction) => {
+        restaurantsService.get(
+            req.query.booked as string as "All" | "Full" | "NotFull",
+            req.query.from as string,
+            req.query.to as string,
+            req.query.sort as string as "ASC" | "DESC",
+            req.query.page as unknown as number,
+            req.query.limit as unknown as number,
+        ).then((data: Restaurant[] | null) => {
             if (data && data.length > 0) {
                 res.status(200).json(data);
             }
@@ -17,10 +26,36 @@ class RestaurantController {
         });
     }
 
+    public static getById = (req: Request, res: Response, next: NextFunction) => {
+        restaurantsService.getById(req.params.id as unknown as number).then((data: Restaurant | null) => {
+            if (data) {
+                res.status(200).json(data);
+            }
+            else {
+                res.status(404).json({ message: 'No restaurant with that id found' });
+            }
+        }).catch((err: Error) => {
+            next(new Error('Error occurred')); // TODO: handle error
+        });
+    }
+
     public static addNew = (req: Request, res: Response, next: NextFunction) => {
-        restaurantsService.add(req.body).then((data: any) => {
+        restaurantsService.add(req.body).then((data: Restaurant | null) => {
             if (data) {
                 res.json('Restaurant saved succesfully');
+            }
+        }).catch((err: Error) => {
+            next(new Error('Error occurred')); // TODO: handle error
+        })
+    }
+
+    public static delete = (req: Request, res: Response, next: NextFunction) => {
+        restaurantsService.delete(req.params.id as unknown as number).then((data: true | null) => {
+            if (data) {
+                res.json('Restaurant deleted succesfully');
+            }
+            else {
+                res.status(404).json({ message: 'No restaurant with that id found' });
             }
         }).catch((err: Error) => {
             next(new Error('Error occurred')); // TODO: handle error
