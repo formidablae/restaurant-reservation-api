52a4b51abf947f30053f9a2a10feaac2af16fb06
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-08 22:09:34 +0100

    Made a onetomany relation between restaurants and their reservations. Included service from previous commit

--- src/app/models/Restaurant.ts
+++ src/app/models/Restaurant.ts
@@ -1,3 +1,6 @@
+import { Reservation } from "./entities/Reservation";
+
 export interface IRestaurant {
     name: string;
+    reservations: Reservation[];
 }
--- src/app/models/entities/Restaurant.ts
+++ src/app/models/entities/Restaurant.ts
@@ -1,18 +1,24 @@
 import { IsNotEmpty } from 'class-validator';
 import {
     Column, CreateDateColumn, Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
     UpdateDateColumn
 } from 'typeorm';
+import { IRestaurant } from '../Restaurant';
+import { Reservation } from './Reservation';
 
 @Entity()
-export class Restaurant {
+export class Restaurant implements IRestaurant {
     @PrimaryGeneratedColumn()
     public id!: number;
 
     @Column()
     @IsNotEmpty()
     public name!: string;
+
+    @OneToMany(type => Reservation, reservation => reservation.restaurant_id)
+    reservations: Reservation[];
 
     @Column()
     @CreateDateColumn()
