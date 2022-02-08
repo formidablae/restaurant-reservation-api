7d47798bd7141e26bad8301ccd5710f4c47be170
formidablae <81068781+formidablae@users.noreply.github.com>
2022-02-08 22:13:10 +0100

    Updated user routes to include getbyid and delete functionalities

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,9 @@ import UserController from '../app/controllers/UserController';
 
 const router = Router();
 
-router.get('/', UserController.listAll);
+router.get('/', UserController.getFiltered);
+router.get('/:id', UserController.getById);
 router.post('/', UserController.addNew);
+router.delete('/:id', UserController.delete);
 
 export default router;
